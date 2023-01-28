8e002f6a3887652e0678939daa8ff1dba03ef180
Kevin Zhu <kzhu4@gmu.edu>
2023-01-28 13:14:39 -0500

    switched to tqdm

--- experiment_eon1.py
+++ experiment_eon1.py
@@ -1,9 +1,13 @@
 from multiprocessing import Pool, TimeoutError
+from tqdm.contrib.concurrent import process_map
+import sys
 import neuro
 import eons
 import random
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import argparse
+import os
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', "--threads", default=4, type=int)
@@ -65,16 +69,17 @@ pop = evolver.generate_population(eons_params, 1)
 
 
 def fitness(props):
-    import ricky
+    import ricky as sim
     global runsi
     # print(*props, end='\t')
-    r = ricky.Run(10, *props, visible=False)
+    r = sim.Run(10, *props, visible=False)
+    graph = []
     for i in range(9999):
         r.step()
-    loss = r.circleness()
+        graph.append(r.circleness())
     # print(loss)
-    ricky.turtle.getscreen().bye()
-    print('▬', end='', flush=True)
+    loss = sum(graph[-5000:])
+    sim.turtle.getscreen().bye()
     return loss
 
 
@@ -93,33 +98,85 @@ def run_batch(networks):
         ))
 
     # print(dill.detect.baditems(networks))
-    with Pool(processes=args.threads) as pool:
-        results = pool.map(fitness, pop_props)
-    return results
+    # with Pool(processes=args.threads) as pool:
+    #     results = pool.map(fitness, pop_props)
+    return process_map(fitness, pop_props, max_workers=args.threads)
 
 
 print("Running", eons_params["population_size"])
 
 vals = []
-for i in range(500):
-    print(f"[{'-' * 100}]", end='', flush=True)
-    print('\b' * 101, end='', flush=True)    # Calculate the fitnesses of all of the networks in the population
-    fitnesses = run_batch(pop.networks)
+
+
+def do_epoch(pop):
+    str1 = f"Epoch {i:4d}"
+    print(str1)
+
+    start_time = time.time()
+    fitnesses = run_batch(pop.networks)    # Calculate the fitnesses of all of the networks in the population
+
+    # https://en.wikipedia.org/wiki/ANSI_escape_code
+    sys.stdout.write(f"\033[1F\033[2K\033[1G")  # Cursor up one line, clear line, cursor to beginning of line
+    sys.stdout.write(f"\033[1F\033[2K\033[1G")  # Cursor up one line, clear line, cursor to beginning of line
+    elapsed = time.time() - start_time
+    print(str1, f" completed in {elapsed:.0f} seconds", end='\033[1E')
     # fitnesses = [fitness(g.network) for g in pop.networks]
     # Print information about the best network
-    print('\033[2K\033[1G', end='', flush=True)
     max_fit = max(fitnesses)
     vals.append(max_fit)
     index = fitnesses.index(max_fit)
     gmax = pop.networks[index].network
     if (i % 1 == 0):
-        print("Epoch\t{:4d}, Best Score: {}".format(i, max_fit))
-        print("\t   FOV: {0:3.0f}\tRange: {1:6.3f}\tTurning: {2:6.3f}\tSpeed: {3:6.3f}\v".format(
+        str1 = f"Best Score: {max_fit:8.3g}"
+        str2 = "\t   FOV: {:3.0f}\tRange: {:6.3f}\tTurning: {:6.3f}\tSpeed: {:6.3f}\v".format(
             gmax.get_node(0).get("x1"),
             gmax.get_node(0).get("x2"),
             gmax.get_node(0).get("x3"),
             gmax.get_node(0).get("x4"),
-        ))
+        )
+        print(str1, str2)
+        with open('experiment_eon1.checkpoint.json', 'w') as f:
+            f.write(str(pop.as_json(True)))
+
+        # fig, axs = plt.subplots(2, 1)
+        # axs[0].hist(fitnesses, bins='auto', range=(min(fitnesses), 0))
+        # axs[0].set_title(f"Epoch {i}: Fitness Histogram")
+        # axs[0].set_xlabel("Loss")
+        # axs[0].set_ylabel("Occurences")
+        # axs[1].hist(fitnesses, bins='auto', range=(-500, 0))
+        # axs[1].set_xlabel("Loss")
+        # axs[1].set_ylabel("Occurences")
+
+        # try:
+        #     os.remove('losses.pdf.old')
+        # except (FileNotFoundError, OSError):
+        #     pass
+        # finally:
+        #     try:
+        #         os.rename('losses.pdf', 'losses.pdf.old')
+        #     except (FileNotFoundError, OSError):
+        #         pass
+        # print("Backed up graph")
+        # fig.savefig('losses.pdf')
+        # print("saved graph")
+        # fig.show()  # Causes ioctl error in WSL
+        # print("showed plot")
+
+        # f = open('experiment_eon1.log.txt', 'a')
+        # print("Opened log")
+        # f.write('\n'.join((time.strftime("%Y%m%d %X"), str1, str2, str(fitnesses), '\n')))
+        # print("Wrote to log")
+        # f.close()
+        # print("closed log")
 
     # Create the next population based on the fitnesses of the current population
-    pop = evolver.do_epoch(pop, fitnesses, eons_params)
+    return evolver.do_epoch(pop, fitnesses, eons_params)
+    print("evolved")
+
+
+try:
+    for i in range(500):
+        pop = do_epoch(pop)
+except KeyboardInterrupt:
+    print("Stopping...", flush=True)
+    raise
