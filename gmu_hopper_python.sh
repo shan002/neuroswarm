089fd1fa07fbf7561b327c5851612ccad2945a16
Kevin Zhu <kzhu4@gmu.edu>
2024-07-25 15:30:54 -0400

    script to install pyenv & python build deps on hopper cluster

---
+++ gmu_hopper_python.sh
@@ -0,0 +1,66 @@
+set -e  # exit on error
+
+mkdir -p ~/.local/packages
+cd ~/.local/packages
+PACKAGES=`realpath 'packages'`
+
+module load automake
+module load autotools
+
+cd ~/.local
+wget https://ftp.wayne.edu/gnu/autoconf/autoconf-2.72.tar.xz
+tar -xf autoconf-2.72.tar.xz
+cd autoconf-2.72
+./configure --prefix $PACKAGES/autoconf-2.72
+make && make install
+
+module load use.own
+module load autoconf-2.72
+
+cd ~/.local
+wget https://github.com/libffi/libffi/releases/download/v3.4.6/libffi-3.4.6.tar.gz
+tar -xf libffi-3.4.6.tar.gz
+cd libffi-3.4.6
+./configure --prefix $PACKAGES/libffi-3.4.6
+make && make install
+
+cd
+
+module load use.own
+module load libffi-3.4.6
+
+if ! command -v pyenv &> /dev/null
+then
+	curl https://pyenv.run | bash
+
+	echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+	echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+	echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
+	source ~/.bashrc
+fi
+
+pyenv doctor
+
+pyenv install 3.12.4 --force
+
+pyenv global 3.12.4
+
+python --version
+pip --version
+
+mkdir -p ~/neuromorphic/framework
+cd ~/neuromorphic/framework
+pip install virtualenv
+python -m virtualenv pyframework
+source pyframework/bin/activate
+pip install uv
+alias pip='uv pip'
+shopt -s expand_aliases
+pip install -r requirements.txt
+./scripts/create_env.sh
+cd ~/neuromorphic/RobotSwarmSimulator
+pip install -e .
+pip install -r mindeps.txt
+pip install tqdm circle_fit
+unalias pip
