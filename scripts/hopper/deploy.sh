8fc529a2fbc004ee90922fef4a13648ec805fb57
Kevin Zhu <kzhu4@gmu.edu>
2025-01-15 22:25:27 -0500

    add deploy.sh comments

--- scripts/hopper/deploy.sh
+++ scripts/hopper/deploy.sh
@@ -1,39 +1,58 @@
 #!/bin/bash
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-cd "$parent_path"
+cd "$parent_path"  # cd to the location of this script
 
-set -e  # exit on error
+set -e  # EXIT ON ERROR
 
 mkdir -p ~/privatemodules
-cp -Rf privatemodules/** ~/privatemodules
+cp -Rf privatemodules/** ~/privatemodules  # copy privatemodules/ to $HOME
 
+# make project directory
 mkdir -p ~/neuromorphic
 cd ~/neuromorphic
 
+# download the relevant repositories
 if [ ! -d ~/neuromorphic/RobotSwarmSimulator ]; then
   git clone https://github.com/kenblu24/RobotSwarmSimulator.git
 fi
 
+# This next bit downloads the TENNLAB framework.
+# You'll need to have your SSH key in bitbucket for this to work.
+# You may need to generate a new `id_rsa` or `id_ed25519` key pair
+# and modify ~/.ssh/config for git to use the correct key.
+#
+# Here's an example ~/.ssh/config file:
+#
+# Host *
+#    IdentityFile ~/.ssh/id_rsa
+#    StrictHostKeyChecking=no
+#    UserKnownHostsFile ~/.ssh/known_hosts
+#
 if [ ! -d ~/neuromorphic/framework ]; then
   git clone git@bitbucket.org:neuromorphic-utk/framework.git
 fi
-
 if [ ! -d ~/neuromorphic/framework/processors/caspian ]; then
   git clone git@bitbucket.org:neuromorphic-utk/caspian.git ~/neuromorphic/framework/processors/caspian
 fi
 
-
+# make a spot for our python local dependencies to go
 mkdir -p ~/.local/packages
 cd ~/.local/packages
 PACKAGES=`realpath '.'`
 
+# luckily hopper has these dependencies available already; no need to build them
+# let's load them now
 module load automake
 module load autotools
 module load readline
 module load bzip2
 module load sqlite
 
+# sadly autoconf and libffi are dependencies for python.
+# libffi is needed for _ctypes which is needed for scipy
+
+# download and build autoconf-2.72
 cd ~/.local
 wget https://ftp.wayne.edu/gnu/autoconf/autoconf-2.72.tar.xz
 tar -xf autoconf-2.72.tar.xz
@@ -41,9 +60,10 @@ cd autoconf-2.72
 ./configure --prefix $PACKAGES/autoconf-2.72
 make && make install
 
-module load use.own
+module load use.own  # load this to update the available user modules
 module load autoconf-2.72
 
+# download and build libffi
 cd ~/.local
 wget https://github.com/libffi/libffi/releases/download/v3.4.6/libffi-3.4.6.tar.gz
 tar -xf libffi-3.4.6.tar.gz
@@ -51,14 +71,15 @@ cd libffi-3.4.6
 ./configure --prefix $PACKAGES/libffi-3.4.6
 make && make install
 
-cd
-
-module load use.own
+module load use.own  # load this to update the available user modules
 module load libffi-3.4.6
 
+cd
+
+# download and install pyenv if we don't have it
 if ! command -v pyenv &> /dev/null
 then
-	rm -rf ~/.pyenv
+	rm -rf ~/.pyenv  # if ~/.pyenv exists, the install script will error
 
 	curl https://pyenv.run | bash
 
@@ -69,38 +90,51 @@ then
 	source ~/.bashrc
 fi
 
-pyenv doctor
+pyenv doctor  # check if pyenv thinks we can build python
 
 pyenv install 3.12.4 --force
 
-pyenv global 3.12.4
+pyenv global 3.12.4  # load our newly-built python
 
+# make sure we can run python and pip
 python --version
 pip --version
 
+set -x  # print commands
+# check that we have _ctypes for scipy
+python -c 'import _ctypes'
+set +x
+
+# make the pyframework virtual environment manually
+# the framework/scripts/create_env.sh script uses `venv`` which is less versatile
+# so we run `virtualenv`` ourselves to make it ahead of time
 mkdir -p ~/neuromorphic/framework
 cd ~/neuromorphic/framework
 pip install virtualenv
 python -m virtualenv pyframework
 source pyframework/bin/activate
-pip install uv
-shopt -s expand_aliases
-alias pip='uv pip'
-source ./scripts/create_env.sh
-if [ -z ${VIRTUAL_ENV+x} ];  # if no virtual environment detected
+pip install uv  # uv speeds package install up a LOT but sometimes errors when regular pip still works
+shopt -s expand_aliases  # make aliases work in our shell
+alias pip='uv pip'  # use uv instead of pip for the following python package installs
+source ./scripts/create_env.sh  # install framework and its dependencies
+if [ -z ${VIRTUAL_ENV+x} ];  # if no virtual environment detected (if create_env deactivated the venv)
 then
 	source pyframework/bin/activate
 fi
+
+# install RSS and its dependencies
 cd ~/neuromorphic/RobotSwarmSimulator
 pip install -r mindeps.txt
 pip install -e .
 
+# install other dependencies
 cd ~/neuromorphic/turtwig
 pip install -r requirements.txt
 cd ~/neuromorphic
 unalias pip
 
 set -x  # print commands
+# check that these work
 python -c 'import neuro'
 python -c 'import caspian'
 python -c 'import novel_swarms'
