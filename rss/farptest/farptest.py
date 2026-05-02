f98e0518ceb4200323e42fcaff5360674ce213a6
kenblu24 <kenblu24@hotmail.com>
2026-05-02 07:49:47 -0400

    test scenario

--- rss/farptest/farptest.py
+++ rss/farptest/farptest.py
@@ -1,4 +1,9 @@
+import copy
 from pathlib import Path
+from collections import Counter
+
+import numpy as np
+from tqdm.contrib.concurrent import process_map
 
 from swarmsim.config import register_dictlike_type, register_agent_type
 from swarmsim.agent.MazeAgent import MazeAgentConfig
@@ -16,12 +21,52 @@ config = RectangularWorldConfig.from_yaml(cwd / "world.yaml")
 # gui = TennlabGUI(x=0, y=0, h=0, w=300)
 # gui.position = "sidebar_right"
 
-simargs = dict(
-    world_config=config,
-    subscribers=[],
-    # gui=gui,
-    show_gui=True,
-    start_paused=True,
-)
-world = simulator(**simargs, stop_detection=lambda w: w.metrics[0].current_value)  # run simulator
-print(world.metrics[0].current_value)
+
+def stop(world):
+    return any(m.current_value for m in world.metrics)
+
+
+def test_single(config):
+    tempconfig = copy.deepcopy(config)
+    # config.seed += i
+    stats = Counter()
+    world = simulator(
+        world_config=tempconfig,
+        subscribers=[],
+        # gui=gui,
+        show_gui=False,
+        start_paused=False,
+        stop_detection=stop,
+    )  # run simulator
+    for m in world.metrics:
+        stats[m.name] += m.current_value
+    return stats
+
+
+def test_mp(n=100):
+    configs = []
+    seeds = np.random.default_rng(getattr(config, "seed", None)).integers(0, 2**31, size=n)
+    for i in range(100):
+        tempconfig = copy.deepcopy(config)
+        tempconfig.seed = seeds[i]
+        configs.append(tempconfig)
+    results = process_map(test_single, configs)
+    print(sum(results, Counter()))
+
+
+def run():
+    world = simulator(
+        world_config=config,
+        subscribers=[],
+        # gui=gui,
+        show_gui=True,
+        start_paused=True,
+        # stop_detection=stop,
+    )  # run simulator
+    for m in world.metrics:
+        print(f"{m.name}: {m.current_value}")
+
+
+if __name__ == "__main__":
+    test_mp()
+    # run()
